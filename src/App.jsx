78b8941cbae81c3456cadde6405621e005ff808a
ValenGu1t0 <devalen543@gmail.com>
2025-02-25 23:14:09 -0300

    Inicio Proyecto Tienda Web React

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// Forma lenta, pero permite importar de a muchas cosas
+import { Button } from "@mui/material";
+
+// Forma rapida, pero solo permite seleccionar un Componente
+/* import Button from "@mui/material/Button" */
+
+
+function App() {
+
+    return (
+
+        <div className="bg-red-200">
+          <Button>Boton MUI</Button>
+        </div>
+
+    );
+}
+
+export default App;
